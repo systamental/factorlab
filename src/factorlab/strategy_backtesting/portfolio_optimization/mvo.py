c9df7af56c15ebf7640b22541f2a18fda16260e7
Nick Latinovic <nicklatinovic@gmail.com>
2024-06-02 20:37:45 +0300

    feature: added 6 mean variance optimization methods

--- src/factorlab/strategy_backtesting/portfolio_optimization/mvo.py
+++ src/factorlab/strategy_backtesting/portfolio_optimization/mvo.py
@@ -36,11 +36,11 @@ class MVO:
     """
     def __init__(self,
                  returns: Union[pd.DataFrame, pd.Series],
-                 method: str = 'min_volatility',
-                 max_weight: float = 1,
-                 min_weight: float = 0,
-                 budget: float = 1,
-                 risk_aversion: float = 1,
+                 method: str = 'min_vol',
+                 max_weight: float = 1.0,
+                 min_weight: float = 0.0,
+                 budget: float = 1.0,
+                 risk_aversion: float = 1.0,
                  risk_free_rate: Optional[float] = None,
                  asset_names: Optional[List[str]] = None,
                  exp_ret_method: Optional[str] = None,
@@ -56,8 +56,8 @@ class MVO:
         ----------
         returns: pd.DataFrame
             The returns of the assets or strategies. If not provided, the returns are computed from the prices.
-        method: str, {'min_volatility', 'target_return_min_volatility', 'max_sharpe_ratio', 'max_return_min_vol',
-        'max_diversification_ratio', 'efficient_return', 'efficient_risk'}, default 'min_volatility'
+        method: str, {'min_vol', 'max_return_min_vol', 'max_sharpe', 'max_diversification', 'return_targeting',
+        'risk_budgeting'}, default 'min_vol'
             Optimization method to compute weights.
         max_weight: float, default 1
             Maximum weight of the asset.
@@ -114,14 +114,15 @@ class MVO:
         """
         Preprocesses data.
         """
-        # ret
+        # returns
         if not isinstance(self.returns, pd.DataFrame) and not isinstance(self.returns, pd.Series):  # check data type
             raise ValueError('rets must be a pd.DataFrame or pd.Series')
         if isinstance(self.returns, pd.Series):  # convert to df
             self.returns = self.returns.to_frame()
         if isinstance(self.returns.index, pd.MultiIndex):  # convert to single index
-            self.returns = self.ret.unstack()
+            self.returns = self.returns.unstack()
         self.returns.index = pd.to_datetime(self.returns.index)  # convert to index to datetime
+        self.returns = self.returns.dropna()  # drop na
 
         # method
         if self.method not in ['min_vol', 'max_return_min_vol', 'max_sharpe', 'max_diversification',
@@ -234,7 +235,16 @@ class MVO:
         """
         Get constraints.
         """
-        if self.method == 'return_targeting':
+        if self.method == 'max_sharpe':
+            self.constraints = [
+                cp.sum((self.exp_ret - self.risk_free_rate).T @ self.y) == self.budget,
+                cp.sum(self.y) == self.k,
+                self.k >= 0,
+                self.y <= self.k * self.max_weight,
+                self.y >= self.k * self.min_weight
+            ]
+
+        elif self.method == 'return_targeting':
             if self.target_ret is None:
                 raise ValueError('Target return is required for this method.')
             else:
@@ -255,15 +265,6 @@ class MVO:
                     cp.sum(self.weights) == self.budget,
                     self.portfolio_risk <= self.target_risk
                 ]
-
-        elif self.method == 'max_sharpe':
-            self.constraints = [
-                cp.sum((self.exp_ret - self.risk_free_rate).T @ self.y) == self.budget,
-                cp.sum(self.y) == self.k,
-                self.k >= 0,
-                self.y <= self.k * self.max_weight,
-                self.y >= self.k * self.min_weight
-            ]
 
         else:
             self.constraints = [
@@ -361,144 +362,3 @@ class MVO:
         self.weights = self.check_weights()
 
         return self.weights
-
-    def compute_portfolio_metrics(self):
-        """
-        Get portfolio metrics.
-        """
-        pass
-        # # portfolio return
-        # self.portfolio_rets = self.weights.value.T @ self.exp_rets
-        # # portfolio risk
-        # self.portfolio_risk = np.sqrt(self.weights.value.T @ self.cov_matrix @ self.weights.value)
-
-
-# def mean_variance_optimize(μ, Σ, vol_tgt=0.05, Tht=None):
-#     """
-#
-#     :param μ: Return vector
-#     :param Σ: Covariance matrix
-#     :param vol_tgt: Volatility target
-#     :param Tht: (Optional) constraint matrix. The optimization enforces w.T @ Θ = 0
-#     :return: Optimal portfolio weights
-#     """
-#
-#     # reshape μ if necessary and check dimensions are consistent
-#     μ = np.atleast_2d(μ).T
-#     assert (Σ.shape == (μ.shape[0], μ.shape[0]))
-#
-#     if Tht is None:
-#         # default mean-variance optimizer
-#         Σi = np.linalg.inv(Σ)
-#         α = np.sqrt(μ.T @ Σi @ μ) / vol_tgt
-#         return (Σi @ μ) / α
-#     else:
-#         Tht = np.atleast_2d(Tht)
-#         assert (μ.shape[0] == Tht.shape[0])
-#         if Tht.shape[1] == 1:
-#             # single constraint on the portfolio weights
-#             Σi = np.linalg.inv(Σ)
-#             d_μ_μ = μ.T @ Σi @ μ
-#             d_μ_Θ = μ.T @ Σi @ Tht
-#             d_Θ_Θ = Tht.T @ Σi @ Tht
-#
-#             β = d_μ_Θ / d_Θ_Θ
-#             α = np.sqrt((d_μ_μ * d_Θ_Θ - d_μ_Θ * d_μ_Θ) / d_Θ_Θ) / vol_tgt
-#             return Σi @ (μ - β * Tht) / α
-#         else:
-#             # perform an svd of the constraint matrix...
-#             u, d, vt = np.linalg.svd(Tht, full_matrices=True)
-#             # ...and find the basis for its null space
-#             p = u[:, Tht.shape[1]:]
-#
-#             # The require portfolio weight matrix w is now given by w = p @ w_p
-#             # for some vector w_p i.e. it lies in the null space of Θ. Therefore,
-#             # we can solve a simple mean-variance optimization in the null space
-#             # by using projected versions of the return and covariance matrices.
-#             # These are calculated below and the estimated w_p is converted back
-#             # into the portfolio space by left multiplying by p
-#             μ_p = p.T @ μ
-#             Σ_p = p.T @ Σ @ p
-#             Σi_p = np.linalg.inv(Σ_p)
-#
-#             α = np.sqrt(μ_p.T @ Σi_p @ μ_p) / vol_tgt
-#             return (p @ Σi_p @ μ_p) / α
-#
-#
-# def robust_regression_cqp(A, b, γ):
-#     '''
-#     Implementation of robust regression with Huber estiamtors as described in:
-#     "Robust Linear and Support Vector Regression" Mangasarian and Musicant
-#
-#     Given A, b we are trying to solve for x which minimizes
-#                 Σ ρ((Ax-b)[i])
-#     where ρ is the Huber M-estimator.
-#
-#     This is solved by solving the convex quadratic program (1):
-#                 min z'z/2 + γ e' (r + s)
-#                 s.t. Ax-b-z=r-s
-#                      r,s >= 0
-#
-#     Cvxopt solves problems of the form (2):
-#                 min x'Px/2 + q'x
-#                 s.t. Gx <= h
-#                      Cx = d
-#
-#     We rewrite the constraints above in terms of P, q, G, h, C and d. Note that
-#     this will solve for x, z, r and s from (1). The function splits and returns this
-#     '''
-#     A, b = np.atleast_2d(A, b)  # ensure they are 2d matrices
-#     t, l = A.shape  # check shapes match
-#     assert (b.shape[0] == t)
-#     assert (γ > 0)
-#
-#     n = l + 3 * t  # size of the solution vector for convex optimization
-#
-#     P = np.diag(np.hstack([np.zeros(l), np.ones(t), np.zeros(2 * t)]))
-#     q = np.zeros((n, 1))
-#     q[l + t:] = γ
-#     G = -np.diag(np.sign(q.reshape(q.shape[0])))
-#     h = np.zeros((n, 1))
-#     C = np.hstack([A, -np.eye(t), -np.eye(t), np.eye(t)])
-#
-#     P = matrix(P)
-#     q = matrix(q)
-#     G = matrix(G)
-#     h = matrix(h)
-#     C = matrix(C)
-#     d = matrix(b)
-#
-#     sol = solvers.qp(P, q, G, h, C, d)
-#
-#     y = np.array(sol['x'])
-#
-#     x = y[:l]
-#     z = y[l:l + t]
-#     r = y[l + t:l + 2 * t]
-#     s = y[l + 2 * t:]
-#
-#     return x, z, r, s
-#
-#
-# def robust_mvo_huber(X, r=None, scale=None):
-#     '''
-#     Performs MVO by treating it as a regression. However, we use
-#     the robust SVM function to perform a robust regression.
-#     '''
-#     T, k = X.shape
-#
-#     if r is None:
-#         r = np.mean(X, axis=0, keepdims=True).T
-#     else:
-#         r = np.atleast_2d(r)
-#         assert (r.shape == (k, 1))
-#
-#     b = np.ones((T, 1))
-#
-#     if scale is None:
-#         scale = robust.mad(b - X @ np.linalg.inv(X.T @ X) @ X.T @ b, c=1.)
-#     else:
-#         assert (scale > 0)
-#
-#     w, z, r, s = robust_regression_cqp(X, b, scale)
-#     return w / np.sum(w)
