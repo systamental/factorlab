3016ce71cd3c898e5578f2725874696bdca66b7e
Nick Latinovic <nicklatinovic@gmail.com>
2025-10-05 14:54:58 -0400

    added import paths for value factors to init file

---
+++ src/factorlab/factors/value/__init__.py
@@ -0,0 +1,8 @@
+from factorlab.factors.value.value_ratio import ValueRatio
+from factorlab.factors.value.value_residual import ValueResidual
+
+
+__all__ = [
+    "ValueRatio",
+    "ValueResidual"
+]
