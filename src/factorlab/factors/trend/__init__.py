71ee80fc42b50ee3bc9a99696a3d02f5bade366a
Nick Latinovic <nicklatinovic@gmail.com>
2025-10-06 11:58:51 -0400

    feat: added init file with trend factor paths for easier import

---
+++ src/factorlab/factors/trend/__init__.py
@@ -0,0 +1,36 @@
+from factorlab.factors.trend.adx import ADX
+from factorlab.factors.trend.idio_ewma import IdiosyncraticEWMA
+from factorlab.factors.trend.alpha_momentum import AlphaMomentum
+from factorlab.factors.trend.breakout import Breakout
+from factorlab.factors.trend.divergence import Divergence
+from factorlab.factors.trend.energy import Energy
+from factorlab.factors.trend.ewma import EWMA
+from factorlab.factors.trend.intensity import Intensity
+from factorlab.factors.trend.mw_difference import MWDifference
+from factorlab.factors.trend.price_acceleration import PriceAcceleration
+from factorlab.factors.trend.price_momentum import PriceMomentum
+from factorlab.factors.trend.rsi import RSI
+from factorlab.factors.trend.snr import SNR
+from factorlab.factors.trend.stochastic import Stochastic
+from factorlab.factors.trend.time_trend import TimeTrend
+from factorlab.factors.trend.triple_ewma_diff import TripleEWMADifference
+
+
+__all__ = [
+    "Breakout",
+    "PriceMomentum",
+    "EWMA",
+    "Divergence",
+    "TimeTrend",
+    "PriceAcceleration",
+    "AlphaMomentum",
+    "IdiosyncraticEWMA",
+    "RSI",
+    "Stochastic",
+    "Intensity",
+    "MWDifference",
+    "TripleEWMADifference",
+    "Energy",
+    "SNR",
+    "ADX"
+]
