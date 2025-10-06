a0afcf4ba85ccd8f181c379c84a5252ce6d5678c
Nick Latinovic <nicklatinovic@gmail.com>
2025-10-06 18:50:49 -0400

    feat: added skew init file for easy skew factor import paths

---
+++ src/factorlab/factors/skew/__init__.py
@@ -0,0 +1,14 @@
+from factorlab.factors.skew.skewness import Skew as SkewnessFactor
+from factorlab.factors.skew._max import Max
+from factorlab.factors.skew._min import Min
+from factorlab.factors.skew.max_min import MaxMin
+from factorlab.factors.skew.idio_skew import ISkew
+
+
+__all__ = [
+    "SkewnessFactor",
+    "Max",
+    "Min",
+    "MaxMin",
+    "ISkew"
+]
