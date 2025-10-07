fc5899a088a75fdb34a5b1cce6e88d02d24d8d61
Nick Latinovic <nicklatinovic@gmail.com>
2025-10-06 21:28:45 -0400

    feat: added init file with import paths to reversal factors

---
+++ src/factorlab/factors/reversal/__init__.py
@@ -0,0 +1,12 @@
+from factorlab.factors.reversal.mw_deviation import MWDeviation
+from factorlab.factors.reversal.linear_deviation import LinearDeviation
+from factorlab.factors.reversal.idio_deviation import IdiosyncraticDeviation
+from factorlab.factors.reversal.rsi_detrended import RSIDetrended
+
+
+__all__ = [
+    "MWDeviation",
+    "LinearDeviation",
+    "IdiosyncraticDeviation",
+    "RSIDetrended",
+]
