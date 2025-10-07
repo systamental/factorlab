6bb7ff5cef3146398b50278c71336933636c75cb
Nick Latinovic <nicklatinovic@gmail.com>
2025-10-07 14:19:15 -0400

    fix: removed Roll estimator

--- src/factorlab/factors/liquidity/__init__.py
+++ src/factorlab/factors/liquidity/__init__.py
@@ -1,8 +1,7 @@
 from factorlab.factors.liquidity.high_low_spread import HighLowSpreadEstimator
 from factorlab.factors.liquidity.amihud import Amihud
-from factorlab.factors.liquidity.roll import RollEstimator
 from factorlab.factors.liquidity.edge import EDGE
 
-__all__ = ["HighLowSpreadEstimator", "EDGE", "Amihud", "RollEstimator"]
+__all__ = ["HighLowSpreadEstimator", "EDGE", "Amihud"]
 
 
