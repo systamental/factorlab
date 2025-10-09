cf9ad1181ce229f1660c2b7d2973ccff56caec9d
Nick Latinovic <nicklatinovic@gmail.com>
2025-10-08 23:03:47 -0400

    added init file with imports paths

---
+++ src/factorlab/signal_generation/__init__.py
@@ -0,0 +1,6 @@
+from factorlab.signal_generation.continuous import ZScoreSignal, QuantileSignal, RankSignal
+from factorlab.signal_generation.discrete import DiscreteZScoreSignal, DiscreteQuantileSignal, DiscreteRankSignal
+
+
+__all__ = ["ZScoreSignal", "QuantileSignal", "RankSignal",
+           "DiscreteZScoreSignal", "DiscreteQuantileSignal", "DiscreteRankSignal"]
