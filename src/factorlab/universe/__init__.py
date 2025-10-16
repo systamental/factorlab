e87087456ffb3041a77e82cf61742c3e34267d05
Nick Latinovic <nicklatinovic@gmail.com>
2025-10-16 09:49:28 -0400

    feat: added init file for universe selection

---
+++ src/factorlab/universe/__init__.py
@@ -0,0 +1,7 @@
+from factorlab.universe.liquidity_filter import LiquidityFilter
+
+__all__ = [
+    'LiquidityFilter',
+]
+
+# Class responsible for dynamically selecting the eligible asset universe (e.g., liquidity, market cap, sector filters).
