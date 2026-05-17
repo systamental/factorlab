d26748597c1903507fc5be7be8d7d59bf70d20a0
Nick Latinovic <nicklatinovic@gmail.com>
2026-05-17 22:03:01 +0200

    fix: corrected trend important paths in init

--- src/factorlab/factors/library/trend/__init__.py
+++ src/factorlab/factors/library/trend/__init__.py
@@ -1,4 +1,19 @@
 from factorlab.factors.library.trend.breakout import Breakout
+from factorlab.factors.library.trend.price_momentum import PriceMomentum
+from factorlab.factors.library.trend.ewma import EWMA
+from factorlab.factors.library.trend.divergence import Divergence
+from factorlab.factors.library.trend.time_trend import TimeTrend
+from factorlab.factors.library.trend.price_acceleration import PriceAcceleration
+from factorlab.factors.library.trend.alpha_momentum import AlphaMomentum
+from factorlab.factors.library.trend.idio_ewma import IdiosyncraticEWMA
+from factorlab.factors.library.trend.rsi import RSI
+from factorlab.factors.library.trend.stochastic import Stochastic
+from factorlab.factors.library.trend.intensity import Intensity
+from factorlab.factors.library.trend.mw_difference import MWDifference
+from factorlab.factors.library.trend.triple_ewma_diff import TripleEWMADifference
+from factorlab.factors.library.trend.energy import Energy
+from factorlab.factors.library.trend.snr import SNR
+from factorlab.factors.library.trend.adx import ADX
 
 __all__ = [
     "Breakout",
