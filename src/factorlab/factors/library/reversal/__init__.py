c0b5839000f777b489958d4b6acf99ecbca33c8e
Nick Latinovic <nicklatinovic@gmail.com>
2026-05-17 22:49:20 +0200

    fix: added correct paths to reversal/ init file

--- src/factorlab/factors/library/reversal/__init__.py
+++ src/factorlab/factors/library/reversal/__init__.py
@@ -1,4 +1,8 @@
 from factorlab.factors.library.reversal.linear_deviation import LinearDeviation
+from factorlab.factors.library.reversal.mw_deviation import MWDeviation
+from factorlab.factors.library.reversal.idio_deviation import IdiosyncraticDeviation
+from factorlab.factors.library.reversal.rsi_detrended import RSIDetrended
+from factorlab.factors.library.reversal.reversal import Reversal
 
 __all__ = [
     "MWDeviation",
